491cb2f61d83fceb2f53935769eb3003b2ef22b4
ekjkkk153 <ekjkkk153@daum.net>
2022-02-28 22:46:41 +0900

    새로운 코드 with tiles

--- TeamProject/src/main/java/com/ilinbun/mulcam/controller/MainController.java
+++ TeamProject/src/main/java/com/ilinbun/mulcam/controller/MainController.java
@@ -2,19 +2,13 @@ package com.ilinbun.mulcam.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/")
 public class MainController {
-	@GetMapping("/")
-	public ModelAndView main() {
-		ModelAndView mv = new ModelAndView("index");
-		mv.addObject("cpage", "main");
-		return mv;
-	}
-	
-	@GetMapping("/brag")
-	public String BragMain() {
-		return "brag/bragmain";
+	@GetMapping({"","/index"})
+	public String Main() {
+		return "main";
 	}
 }
--- TeamProject/src/main/java/com/ilinbun/mulcam/controller/SearchController.java
+++ TeamProject/src/main/java/com/ilinbun/mulcam/controller/SearchController.java
@@ -2,15 +2,13 @@ package com.ilinbun.mulcam.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/search")
 public class SearchController {
-	@GetMapping("/search")
-	public ModelAndView searchpage() {
-		ModelAndView mv = new ModelAndView("index");
-		mv.addObject("cpage", "search/search");
-		return mv;
-		
+	@GetMapping("")
+	public String Main() {
+		return "search/main";
 	}
 }
