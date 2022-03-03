c8745a4d4015ddac8f146815daf703be74795ef8
jan <cjoongn6@naver.com>
2022-03-03 23:13:04 +0900

    .

--- TeamProject/src/main/java/com/ilinbun/mulcam/controller/MainController.java
+++ TeamProject/src/main/java/com/ilinbun/mulcam/controller/MainController.java
@@ -42,7 +42,7 @@ public class MainController {
 	@PostMapping("/join")
 	public String postJoin(User user) throws Exception {
 		userService.makeUser(user);
-		return "redirect:/loginSuccess";
+		return "redirect:/loginSuccess"; 
 	}
 	
 	// 회원가입 완료 폼으로 가는 컨트롤러
@@ -61,25 +61,33 @@ public class MainController {
 	@PostMapping("/login")
 	public String login(@RequestParam(value="user_email")String user_email,
 			@RequestParam(value="user_password") String user_password, Model model, HttpServletResponse response, boolean rememberEmail) throws Exception {
-		
+		System.out.println("1");
 		try {
 			userService.loginUser(user_email, user_password);
+			System.out.println("2");
 			session.setAttribute("user_email", user_email);
+			System.out.println("3");
 			// 쿠키기능(로그인저장)
 			if (rememberEmail) {
 				// 1. 쿠키를 생성
+				System.out.println("4");
 				Cookie cookie = new Cookie("user_email", user_email);
 				// 2. 응답에 저장
 				response.addCookie(cookie);
+				System.out.println("5");
 			} else {
 				// 1. 쿠키를 생성
+				System.out.println("6");
 				Cookie cookie = new Cookie("user_email", user_email);
 				// 쿠키의 유효기간 0으로 설정
+				System.out.println("7");
 				cookie.setMaxAge(0);
 				// 2. 응답에 저장
+				System.out.println("8");
 				response.addCookie(cookie);
 			}
 		} catch(Exception e) {
+			System.out.println("9");
 			e.printStackTrace();
 			String msg = URLEncoder.encode("아이디 또는 비밀번호가 일치하지 않아요!!", "utf-8");
 			return "redirect:/login?msg=" + msg;
