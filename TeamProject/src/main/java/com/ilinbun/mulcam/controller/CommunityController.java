101f33640fabd09f7b8f8beb90353e702db5e9ec
ParkTree <vkzmxmfl@gmail.com>
2022-03-04 10:11:49 +0900

    # WARNING: head commit changed in the meantime
    Merge branch 'dev' of
    https://github.com/choijunkeun/s3g2_teamproject1.git into dev

--- TeamProject/src/main/java/com/ilinbun/mulcam/controller/CommunityController.java
+++ TeamProject/src/main/java/com/ilinbun/mulcam/controller/CommunityController.java
@@ -1,25 +1,356 @@
 package com.ilinbun.mulcam.controller;
 
-import java.util.ArrayList;
+import java.io.File;
+import java.util.List;
 
+import javax.servlet.ServletContext;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
+import com.ilinbun.mulcam.dto.Com_board;
+import com.ilinbun.mulcam.dto.PageInfo;
+import com.ilinbun.mulcam.service.BoardService;
 
-
-@Controller
-@RequestMapping("/comm")
-public class CommunityController {
-	@GetMapping("")
-	public String Main(Model m) {
-		
-		return "community/main";
-	}
+//@Controller
+//@RequestMapping("/comm")
+//public class CommunityController {
+//	@GetMapping("")
+//	public String Main(Model m) {
+//		
+//		return "community/main";
+//	}
+//}
 
 //	@GetMapping("/test")
 //	public String Main() {
 //		return "/community/test";
 //	}
+//}
+@Controller
+@RequestMapping("/comm")
+public class CommunityController {
+	@Autowired
+	private BoardService boardService;
+
+	@Autowired
+	private ServletContext servletContext;
+
+	@GetMapping("")
+	public String Main(Model m) {
+		return "community/main";
+	}
+
+
+	
+	
+	// @@@@@@@@    커뮤니티    @@@@@@@@@
+	
+	@GetMapping("/writeform") // 뒤에 url 주소 적는 곳
+	public String writeform() {
+		return "community/board/writeform";
+	}
+
+	@PostMapping("boardwrite")
+	public ModelAndView boardWrite(@ModelAttribute Com_board board) {
+		ModelAndView mv = new ModelAndView();
+		try {
+			if (!board.getFile().isEmpty()) { // 파일 첨부시 파일 업로드
+				String path = servletContext.getRealPath("/boardupload/");
+				File destFile = new File(path + board.getFile().getOriginalFilename());
+				board.setBoard_filename(board.getFile().getOriginalFilename());
+				// 파일명을 변수에 담은것. 인서트하려고.DB에 넣기위에
+				board.getFile().transferTo(destFile);
+			} // 파일 객체 만들어서 저장 업로드.
+			boardService.regBoard(board);
+			mv.setViewName("redirect:/comm/boardlist");
+		} catch (Exception e) {
+			e.printStackTrace();
+			mv.addObject("err", "새글 등록 실패");
+			mv.setViewName("/community/board/err");
+		}
+		return mv;
+	}
+
+	
+	@RequestMapping(value = "/boardlist", method = { RequestMethod.GET, RequestMethod.POST })
+	public ModelAndView boardList(@RequestParam(value = "page", required = false, defaultValue = "1") int page) {
+		ModelAndView mv = new ModelAndView();
+		PageInfo pageInfo = new PageInfo();
+		try {
+			List<Com_board> articleList = boardService.getBoardList(page, pageInfo);
+			mv.addObject("pageInfo", pageInfo); // 중요함 아무거나 적으면 안됨 똑같아야함
+			mv.addObject("articleList", articleList);
+			mv.setViewName("/community/board/listform");
+		} catch (Exception e) {
+			e.printStackTrace();
+			mv.addObject("err", e.getMessage());
+			mv.setViewName("/community/board/err");
+		}
+		return mv;
+	}
+	
+	
+
+	@GetMapping(value = "/boarddetail")
+	public ModelAndView boardDetail(@RequestParam(value = "board_num") int boardNum,
+			@RequestParam(value = "page", required = false, defaultValue = "1") int page) {
+		ModelAndView mv = new ModelAndView();
+		try {
+			Com_board board = boardService.getBoard(boardNum);
+			mv.addObject("article", board);
+			mv.addObject("page", page);
+			mv.setViewName("/community/board/viewform");
+		} catch (Exception e) {
+			e.printStackTrace();
+			mv.addObject("err", e.getMessage());
+			mv.setViewName("/community/board/err");
+		}
+		return mv;
+	}
+
+	@GetMapping(value = "/replyform")
+	public ModelAndView replyform(@RequestParam(value = "board_num") int boardNum,
+			@RequestParam(value = "page") int page) {
+		ModelAndView mv = new ModelAndView();
+		mv.addObject("page", page);
+		mv.addObject("boardNum", boardNum);
+		mv.setViewName("/community/board/replyform");
+		return mv;
+	}
+
+	@PostMapping(value = "boardreply")
+	public ModelAndView boardreply(@ModelAttribute Com_board board, @RequestParam(value = "page") int page) {
+		ModelAndView mv = new ModelAndView();
+		try {
+			boardService.regReply(board);
+			mv.addObject("page", page);
+			mv.setViewName("redirect:/comm/boardlist");
+		} catch (Exception e) {
+			e.printStackTrace();
+			mv.addObject("err", e.getMessage());
+			mv.setViewName("/community/board/err");
+		}
+		return mv;
+	}
+
+	@GetMapping(value = "modifyform")
+	public ModelAndView modifyform(@RequestParam(value = "board_num") int boardNum) {
+		ModelAndView mv = new ModelAndView();
+		try {
+			Com_board board = boardService.getBoard(boardNum);
+			mv.addObject("article", board);
+			mv.setViewName("/community/board/modifyform");
+		} catch (Exception e) {
+			e.printStackTrace();
+			mv.addObject("err", e.getMessage());
+			mv.setViewName("/community/board/err");
+
+		}
+		return mv;
+	}
+
+	@PostMapping(value = "boardmodify")
+	public ModelAndView boardmodify(@ModelAttribute Com_board board) {
+		ModelAndView mv = new ModelAndView();
+		try {
+			boardService.modifyBoard(board);
+			mv.addObject("board_num", board.getBoard_num());
+			mv.setViewName("redirect:/comm/boarddetail");
+		} catch (Exception e) {
+			e.printStackTrace();
+			mv.addObject("err", e.getMessage());
+			mv.setViewName("/community/board/err");
+		}
+		return mv;
+	}
+
+	@GetMapping(value = "deleteform")
+	public ModelAndView deleteform(@RequestParam(value = "board_num") int boardNum,
+			@RequestParam(value = "page", required = false, defaultValue = "1") int page) {
+		ModelAndView mv = new ModelAndView();
+		mv.addObject("board_num", boardNum);
+		mv.addObject("page", page);
+		mv.setViewName("/community/board/deleteform");
+		return mv;
+	}
+
+	@PostMapping(value = "boarddelete")
+	public ModelAndView boarddelete(@RequestParam(value = "board_num") int boardNum,
+			@RequestParam(value = "board_pass") String boardPass,
+			@RequestParam(value = "page", required = false, defaultValue = "1") int page) {
+		ModelAndView mv = new ModelAndView();
+		try {
+			boardService.removeBoard(boardNum, boardPass);
+			mv.addObject("page", page);
+			mv.setViewName("redirect:/comm/boardlist");
+		} catch (Exception e) {
+			e.printStackTrace();
+			mv.addObject("err", e.getMessage());
+			mv.setViewName("/community/board/err");
+		}
+		return mv;
+	}
+	
+	
+	
+	//@@@@@@@@@@@@@@@   공지사항   @@@@@@@@@@@@@@@@@
+	
+
+	
+	@GetMapping("/writeformN") // 뒤에 url 주소 적는 곳
+	public String writeformN() {
+		return "community/notice/writeformN";
+	}
+
+	@PostMapping("boardwriteN")
+	public ModelAndView boardWriteN(@ModelAttribute Com_board board) {
+		ModelAndView mv = new ModelAndView();
+		try {
+			if (!board.getFile().isEmpty()) { // 파일 첨부시 파일 업로드
+				String path = servletContext.getRealPath("/boardupload/");
+				File destFile = new File(path + board.getFile().getOriginalFilename());
+				board.setBoard_filename(board.getFile().getOriginalFilename());
+				// 파일명을 변수에 담은것. 인서트하려고.DB에 넣기위에
+				board.getFile().transferTo(destFile);
+			} // 파일 객체 만들어서 저장 업로드.
+			boardService.regBoard(board);
+			mv.setViewName("redirect:/comm/boardlistN");
+		} catch (Exception e) {
+			e.printStackTrace();
+			mv.addObject("err", "새글 등록 실패");
+			mv.setViewName("/community/notice/errN");
+		}
+		return mv;
+	}
+
+	
+	@RequestMapping(value = "/boardlistN", method = { RequestMethod.GET, RequestMethod.POST })
+	public ModelAndView boardListN(@RequestParam(value = "page", required = false, defaultValue = "1") int page) {
+		ModelAndView mv = new ModelAndView();
+		PageInfo pageInfo = new PageInfo();
+		try {
+			List<Com_board> articleList = boardService.getBoardList(page, pageInfo);
+			mv.addObject("pageInfo", pageInfo); // 중요함 아무거나 적으면 안됨 똑같아야함
+			mv.addObject("articleList", articleList);
+			mv.setViewName("/community/notice/listformN");
+		} catch (Exception e) {
+			e.printStackTrace();
+			mv.addObject("err", e.getMessage());
+			mv.setViewName("/community/notice/errN");
+		}
+		return mv;
+	}
+	
+	
+
+	@GetMapping(value = "/boarddetailN")
+	public ModelAndView boardDetailN(@RequestParam(value = "board_num") int boardNum,
+			@RequestParam(value = "page", required = false, defaultValue = "1") int page) {
+		ModelAndView mv = new ModelAndView();
+		try {
+			Com_board board = boardService.getBoard(boardNum);
+			mv.addObject("article", board);
+			mv.addObject("page", page);
+			mv.setViewName("/community/notice/viewformN");
+		} catch (Exception e) {
+			e.printStackTrace();
+			mv.addObject("err", e.getMessage());
+			mv.setViewName("/community/notice/errN");
+		}
+		return mv;
+	}
+
+	@GetMapping(value = "/replyformN")
+	public ModelAndView replyformN(@RequestParam(value = "board_num") int boardNum,
+			@RequestParam(value = "page") int page) {
+		ModelAndView mv = new ModelAndView();
+		mv.addObject("page", page);
+		mv.addObject("boardNum", boardNum);
+		mv.setViewName("/community/notice/replyformN");
+		return mv;
+	}
+
+	@PostMapping(value = "boardreplyN")
+	public ModelAndView boardreplyN(@ModelAttribute Com_board board, @RequestParam(value = "page") int page) {
+		ModelAndView mv = new ModelAndView();
+		try {
+			boardService.regReply(board);
+			mv.addObject("page", page);
+			mv.setViewName("redirect:/comm/boardlistN");
+		} catch (Exception e) {
+			e.printStackTrace();
+			mv.addObject("err", e.getMessage());
+			mv.setViewName("/community/notice/errN");
+		}
+		return mv;
+	}
+
+	@GetMapping(value = "modifyformN")
+	public ModelAndView modifyformN(@RequestParam(value = "board_num") int boardNum) {
+		ModelAndView mv = new ModelAndView();
+		try {
+			Com_board board = boardService.getBoard(boardNum);
+			mv.addObject("article", board);
+			mv.setViewName("/community/notice/modifyformN");
+		} catch (Exception e) {
+			e.printStackTrace();
+			mv.addObject("err", e.getMessage());
+			mv.setViewName("/community/notice/errN");
+
+		}
+		return mv;
+	}
+
+	@PostMapping(value = "boardmodifyN")
+	public ModelAndView boardmodifyN(@ModelAttribute Com_board board) {
+		ModelAndView mv = new ModelAndView();
+		try {
+			boardService.modifyBoard(board);
+			mv.addObject("board_num", board.getBoard_num());
+			mv.setViewName("redirect:/comm/boarddetailN");
+		} catch (Exception e) {
+			e.printStackTrace();
+			mv.addObject("err", e.getMessage());
+			mv.setViewName("/community/notice/errN");
+		}
+		return mv;
+	}
+
+	@GetMapping(value = "deleteformN")
+	public ModelAndView deleteformN(@RequestParam(value = "board_num") int boardNum,
+			@RequestParam(value = "page", required = false, defaultValue = "1") int page) {
+		ModelAndView mv = new ModelAndView();
+		mv.addObject("board_num", boardNum);
+		mv.addObject("page", page);
+		mv.setViewName("/community/notice/deleteformN");
+		return mv;
+	}
+
+	@PostMapping(value = "boarddeleteN")
+	public ModelAndView boarddeleteN(@RequestParam(value = "board_num") int boardNum,
+			@RequestParam(value = "board_pass") String boardPass,
+			@RequestParam(value = "page", required = false, defaultValue = "1") int page) {
+		ModelAndView mv = new ModelAndView();
+		try {
+			boardService.removeBoard(boardNum, boardPass);
+			mv.addObject("page", page);
+			mv.setViewName("redirect:/comm/boardlistN");
+		} catch (Exception e) {
+			e.printStackTrace();
+			mv.addObject("err", e.getMessage());
+			mv.setViewName("/community/notice/errN");
+		}
+		return mv;
+	}
+
+	
 }
--- TeamProject/src/main/webapp/WEB-INF/views/community/main.jsp
+++ TeamProject/src/main/webapp/WEB-INF/views/community/main.jsp
@@ -1,7 +1,7 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"%>
 <%@ page import="java.util.*"%>
 <%@ page import="java.text.SimpleDateFormat"%>
-<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
 
 <!DOCTYPE html>
 <html>
@@ -9,58 +9,118 @@
 <meta charset="UTF-8" />
 <title>MVC 게시판</title>
 <style type="text/css">
-h2 {
-	text-align: center;
+.container-comm {
+	margin: 10px auto;
+	padding: 20px auto;
+	background-color: #f6f3f3
+}
+.fa {
+	font-size: 20px;
+}
+.commboard, .notiboard {
+	font-size: 14px;
 }
 
-table {
-	margin: auto;
-	width: 950px;
+a {
+	text-decoration-line: none;
+	color: black;
 }
 
-#tr_top {
-	background: orange;
-	text-align: center;
-}
-
-#pageList {
-	margin: auto;
-	width: 1000px;
-	text-align: center;
-}
-
-#emptyArea {
-	margin: auto;
-	width: 950px;
-	text-align: center;
-}
 </style>
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.8.2/css/all.min.css" />
+
 </head>
 
 <body>
 	<!-- 공지사항 리스트 -->
-	<h2 class="fa fa-exclamation-circle">공지사항 <a href="/comm/notice/writeform">글쓰기</a></h2>
-	<div class="bg-primary p-3">
+	<h2 class="fa fa-exclamation-circle">공지사항 </h2>
+	<button class="btn btn-outline-dark" type="button" style="float:right; "
+	onclick="location.href='comm/boardlistN'">더보기</button>
 	
-		커뮤니티 공지사항
-
+	
+	<div class="container-comm">
+		<!-- Content here -->
+		<!-- 임시 공지사항 -->
+		<div class="container">
+			<div class="column notiboard">
+			
+			<a href="/comm/boardlistN/1" >
+				<div class="row">1. 커뮤니티 게시판 이용가이드1 .1ver
+					<div class="col" style="text-align: right;">2022-02-22 18:20</div>
+				</div>
+			</a>
+			
+			<a href="/comm/boardlistN/2" >
+				<div class="row">2. [Event]나혼자SSul다 - 혼자 살 때의 에피소드를 올려주세요!
+					<div class="col" style="text-align: right;">2022-02-22 18:30</div>
+				</div>
+			</a>
+			
+			<a href="/comm/boardlistN/3" >
+				<div class="row">3. 자취방 구하기 꿀팁
+					<div class="col" style="text-align: right;">2022-02-22 19:28</div>
+				</div>
+			</div>
+			</a>
+		</div>
 	</div>
+
+	<br><br>
 	
+	<h2 class="fa fa-exclamation-circle">커뮤니티 </h2>
+	<button class="btn btn-outline-dark" type="button" style="float:right; "
+	onclick="location.href='comm/boardlist'">더보기</button>
+
 	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+	<div class="container-comm">
+		<!-- Content here -->
+		<div class="container">
+			<div class="row commboard" style="text-align: center;">
+				<div class="col">No</div>
+				<div class="col-5" style="text-align: left;">제목</div>
+				<div class="col" >작성자</div>
+				<div class="col-2" >작성일</div>
+				<div class="col" >조회수</div>
+				<div class="col" >추천수</div>
+			</div>
+			
+			<!-- 임시 게시물 -->
+			<a href="/comm/boardlist/1" >
+				<div class="row commboard" style="text-align: center;">
+					<div class="col">1</div>
+					<div class="col-5" style="text-align: left; ">혼자사는 꿀팁!</div>
+					<div class="col" >혼밥고수</div>
+					<div class="col-2" >2022-02-22 02:28</div>
+					<div class="col" >45</div>
+					<div class="col" >20</div>
+				</div>
+			</a>
+			
+			<a href="/comm/boardlist/2" >
+			<div class="row commboard" style="text-align: center;">
+				<div class="col">2</div>
+				<div class="col-5" style="text-align: left;">종량제 쓰레기 봉투 버릴 때</div>
+				<div class="col" >촤인뀨</div>
+				<div class="col-2" >2022-02-22 02:28</div>
+				<div class="col" >1</div>
+				<div class="col" >2</div>
+			</div>
+			</a>
+		</div>
+	</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 	<!-- 게시판 (미구현) -->
 </body>
 </html>
