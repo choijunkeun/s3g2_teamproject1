89b7e59ad39e509544910e1df119c83e121da0ac
jan <cjoongn6@naver.com>
2022-03-14 23:47:09 +0900

    3/14 23:46 profileImg ing..

--- TeamProject/src/main/java/com/ilinbun/mulcam/controller/MainController.java
+++ TeamProject/src/main/java/com/ilinbun/mulcam/controller/MainController.java
@@ -1,11 +1,15 @@
 package com.ilinbun.mulcam.controller;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.OutputStream;
+import java.io.PrintWriter;
 import java.net.URLEncoder;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
+import java.util.UUID;
 
 import javax.servlet.ServletContext;
 import javax.servlet.http.Cookie;
@@ -14,13 +18,16 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import javax.validation.Valid;
 
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.FileCopyUtils;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -204,7 +211,7 @@ public class MainController {
 	
 	// 정보수정
 	@PostMapping("/infoUpdate")
-	public String infoUpdate(MultipartFile profileImg, String email, String nickname, String originPass, String password, int honbabLevel, HttpSession session) throws Exception {
+	public String infoUpdate(MultipartFile profileImg, String email, String nickname, String originPass, String password, int honbabLevel, HttpSession session, HttpServletRequest request, HttpServletResponse response) throws Exception {
 		String msg = "";
 		System.out.println("infoUpdate() join");
 		User user = (User)session.getAttribute("user");
@@ -248,9 +255,21 @@ public class MainController {
 					profileImgName = user.getProfileImg();
 				} else {
 					String path = servletContext.getRealPath("/profile/");
-					File destFile = new File(path + profileImg.getOriginalFilename());
+					String filename = UUID.randomUUID().toString()+"."+profileImg.getOriginalFilename().substring(profileImg.getOriginalFilename().lastIndexOf('.')+1);
+					File destFile = new File(path + filename);
 					System.out.println(profileImg.isEmpty());
+					PrintWriter writer = null;
+					JSONObject json = new JSONObject();
+					
 					profileImg.transferTo(destFile);
+					writer = response.getWriter();
+					response.setContentType("text/html;charset=utf-8");
+					response.setCharacterEncoding("utf-8");
+					json.append("uploaded", 1);
+					json.append("filename", filename);
+					json.append("url", "/fileview/"+ filename);
+					writer.println(json);
+					
 					System.out.println(path);
 					System.out.println(destFile);
 				}
@@ -268,6 +287,43 @@ public class MainController {
 		}
 		return"redirect:/editInfo";
 	}
+	
+	@GetMapping("/fileview/{filename}")
+	public void fileview(@PathVariable String filename, HttpServletRequest request, HttpServletResponse response) {
+		String path = servletContext.getRealPath("/profile/");
+		File file = new File(path + filename);
+		String sfilename = null;
+		FileInputStream fis = null;
+		
+		try {
+			if (request.getHeader("User-Agent").indexOf("MSIE") > -1) {
+				sfilename = URLEncoder.encode(file.getName(), "utf-8");
+			} else {
+				sfilename = new String(file.getName().getBytes("utf-8"), "ISO-8859-1");
+			}
+			response.setCharacterEncoding("utf-8");
+			response.setContentType("application/octet-stream;charset=utf-8");
+
+			response.setHeader("Content-Disposition", "attachment; filename=" + sfilename);
+			OutputStream out = response.getOutputStream();
+			fis = new FileInputStream(file);
+			FileCopyUtils.copy(fis, out);
+			out.flush();
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			if (fis != null) {
+				try {
+					fis.close();
+				} catch (Exception e) {
+				}
+			}
+		}
+		
+	}
+	
+	
+	
 	
 	//마이페이지에 혼밥자랑 게시글 출력
 	@ResponseBody
